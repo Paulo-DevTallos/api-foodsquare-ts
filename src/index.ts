055c47dbc989662b481201708740b877b2c5c5c5
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-08 16:33:00 -0300

    refactor: implement mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
 import express from 'express';
 import path from 'node:path';
 import http  from 'node:http';
-import mongoose from 'mongoose';
+import { config } from 'dotenv';
 import { Server } from 'socket.io';
 import { router } from './router';
+import { dbConnection } from './database';
 
 const app = express();
+config();
 const server = http.createServer(app)
 export const io = new Server(server)
 
 //mongo db conection - stabelishing connection api when database is up
-mongoose.connect('mongodb://localhost:27017/db-opjs')
-.then(() => {
+dbConnection();
 
-		/*io.on('connection', () => {
-			console.log('Conectado');
-		})*/
-		//habilitando cors
-		app.use((req, res, next) => {
-			res.setHeader('Access-Control-Allow-Origin', '*')
-			res.setHeader('Access-Control-Allow-Methods', '*')
-			res.setHeader('Access-Control-Allow-Headers', '*')
+/*io.on('connection', () => {
+	console.log('Conectado');
+})*/
+//habilitando cors
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', '*')
+	res.setHeader('Access-Control-Allow-Headers', '*')
 
-			next()
-		})
-		app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
-		app.use(express.json())
-		app.use(router);
+	next()
+})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(express.json())
+app.use(router);
 
-		const PORT = 3008
+const port = process.env.PORT || 3020;
 
-		server.listen(PORT, () => {
-			console.log(`Server is running on port http://localhost:${PORT}`)
-		})
-	})
-	.catch(() => console.log('error conection'))
+server.listen(port, () => {
+	console.log(`Server is running on port http://localhost:${port}`)
+})
